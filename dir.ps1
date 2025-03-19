0ffdf83f8afe5762803933e37d600c9040513f85
Jahirul Islam <j.islam.trine@gmail.com>
2025-03-19 14:53:03 +0600

    Added spool.

--- dir.ps1
+++ dir.ps1
@@ -2,9 +2,9 @@ $Timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
 $LogDir = "./log_files"
 
 # Database connection parameters
-$username = "user"
-$password = "pass"
-$connectionString = "ip:port/SID" # TNS entry or host:port/service_name
+$username = "powercard"
+$password = "pcard001"
+$connectionString = "192.168.0.134:1521/pcard" # TNS entry or host:port/service_name
 
 # Get all directories in a specific location
 $directories = Get-ChildItem -Path "./sql_files" -Directory
@@ -16,7 +16,7 @@ foreach ($dir in $directories) {
     # # Example: Run SQL scripts in each directory
     $sqlFiles = Get-ChildItem -Path $dir.FullName -Filter "*.sql"
     foreach ($file in $sqlFiles) {
-        $LogFile = "$LogDir/$dir.$($file.Name)._$Timestamp.log"
+        $LogFile = "$LogDir/$dir_$($file.Name)_$Timestamp.log"
     # Header for log file
 "===== SQL Execution Log - Started $(Get-Date) =====" | Out-File -FilePath $LogFile
 "Directory: $dir" | Out-File -FilePath $LogFile -Append
@@ -27,9 +27,9 @@ Write-Host "Executing: $dir/$($file.Name) at $(Get-Date -Format 'HH:mm:ss')"
 
 # $TempSql = [System.IO.Path]::GetTempFileName() + ".sql"
 $tempFile = New-TemporaryFile
-"SET ECHO ON`nSET SERVEROUTPUT ON SIZE 1000000`n@""$($file.FullName)""`nLIST`nprompt *******************`nEXIT;" | Out-File -FilePath $tempFile -Encoding ASCII
+"SET ECHO ON`nSET SERVEROUTPUT ON SIZE 1000000`nSPOOL ./spool.txt append`n@""$($file.FullName)""`nSPOOL OFF`nEXIT;" | Out-File -FilePath $tempFile -Encoding ASCII
 # Run the SQL file and capture output
-    $output = sqlplus -S "$username/$password@$connectionString" "@$tempFile"
+    $output = sqlplus -L  "$username/$password@$connectionString" "@$tempFile"
    # $output = sqlplus -S "$username/$password@$connectionString" "@$TempSql;"
    # $output = sqlplus "$username/$password@$connectionString"
    $output | ForEach-Object { 
@@ -49,7 +49,7 @@ $tempFile = New-TemporaryFile
        "Status: ERROR (Exit code: $ExitCode)" | Out-File -FilePath $LogFile -Append
     }
         "Completed at $(Get-Date -Format 'HH:mm:ss')" | Out-File -FilePath $LogFile -Append
-    "----------------" | Out-File -FilePath $LogFile -Append
+        "----------------" | Out-File -FilePath $LogFile -Append
 
     Write-Host "Execution completed. See $LogFile for details."
     }
