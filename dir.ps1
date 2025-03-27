f725e2fbde9da83e41e818c597200db5c7093ee1
Jahirul Islam <j.islam.trine@gmail.com>
2025-03-27 14:32:22 +0600

    changed log file generation process.

--- dir.ps1
+++ dir.ps1
@@ -7,19 +7,31 @@ $connectionString = "192.168.0.134:1521/pcard" # TNS entry or host:port/service_
 $LogDir = "./log_files"
 $afterRunDir = './after_run'
 
+#START JS2603B - added a variable to control what folder to run on
+$currentDir = "000_Sequences"
+#END JS2603B
+
 #variables
 $Timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
 
 #Initial setup
 #clear log file dir
 Remove-Item -Path "$LogDir\*"
-#create folders if they do not exist
+#START JS2603C - create folders if they do not exist
 if (!(Test-Path -Path "$LogDir")) {
+    Write-Host "Path not found. Creating $LogDir"
     New-Item -ItemType Directory -Path "$LogDir"
 }
+if (!(Test-Path -Path "$Logdir/$currentDir")) {
+    Write-Host "Path not found. Creating $Logdir/$currentDir"
+    New-Item -ItemType Directory -Path "$Logdir/$currentDir" 
+}
+#END JS2603C
+
 if (!(Test-Path -Path "$afterRunDir")) {
     New-Item -ItemType Directory -Path "$afterRunDir"
 }
+
 
 # Get all directories in a specific location
 $directories = Get-ChildItem -Path "./sql_files" -Directory
@@ -32,7 +44,10 @@ foreach ($dir in $directories) {
     $sqlFiles = Get-ChildItem -Path $dir.FullName -Include "*.sql", "*.pks", "*.pkb" -Recurse | Sort-Object Extension -Descending
     Write-Host $sqlFiles
     foreach ($file in $sqlFiles) {
-        $LogFile = "$LogDir/$dir_$($file.Name)_$Timestamp.log"
+        #START JS2603 - Changes the log file name to include the folder name
+        #$LogFile = "$LogDir/$dir_$($file.Name)_$Timestamp.log"
+        $LogFile = "$LogDir/$dir/$($file.Name)_$Timestamp.log"
+        #END JS2603
     # Header for log file
 "===== SQL Execution Log - Started $(Get-Date) =====" | Out-File -FilePath $LogFile
 "Directory: $dir" | Out-File -FilePath $LogFile -Append
@@ -43,7 +58,10 @@ Write-Host "Executing: $dir/$($file.Name) at $(Get-Date -Format 'HH:mm:ss')"
 
 # $TempSql = [System.IO.Path]::GetTempFileName() + ".sql"
 $tempFile = New-TemporaryFile
+#START JS2603A - changed the command to remove trailing space
+#"SET PACKAGE OFF`nSET ECHO ON`nSET SERVEROUTPUT ON SIZE 1000000`nSPOOL ./spool.txt append`nWHENEVER SQLERROR EXIT SQL.SQLCODE`n@""$($file.FullName)""`n/`nSPOOL OFF`nEXIT;" | Out-File -FilePath $tempFile -Encoding ASCII
 "SET PACKAGE OFF`nSET ECHO ON`nSET SERVEROUTPUT ON SIZE 1000000`nSPOOL ./spool.txt append`nWHENEVER SQLERROR EXIT SQL.SQLCODE`n@""$($file.FullName)""`nSPOOL OFF`nEXIT;" | Out-File -FilePath $tempFile -Encoding ASCII
+#END JS2603A
 # Run the SQL file and capture output
     $output = sqlplus -L  "$username/$password@$connectionString" "@$tempFile"
    # $output = sqlplus -S "$username/$password@$connectionString" "@$TempSql;"
@@ -80,7 +98,6 @@ $tempFile = New-TemporaryFile
     # Write-Host "current file path - $file"
     # Write-Host "Destination file path - $afterRunDir\$folderName\$($file.Name)"
 
-    
     
     }
 }
